c3c6f0c563152921ce2e8e43fdbce204097a0f4c
Deivamani, Vidhyadharan <Vidhyadharan.Deivamani@softwareag.com>
2020-01-20 17:19:12 +0530

    Working for long 10^5 inputs

--- src/com/labkit/vidhya03/QueensAttack.java
+++ src/com/labkit/vidhya03/QueensAttack.java
@@ -18,31 +18,24 @@ public class QueensAttack {
             return count;
         }
 
-        int[][] grid = new int [n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                grid[i][j] =0;
-            }
-        }
-        fillObstacles(grid,obstacles, n);
-        // Fill the queen
+
+       // Fill the queen
 
         r_q = (n-1) - (r_q -1);
         c_q = c_q -1;
 
-        grid[r_q][c_q] = 'Q';
 
 //        printGrid(grid,n);
 
-        count += leftCount(grid, n,r_q ,c_q);
-        count += rightCount(grid, n, r_q, c_q);
-        count += upCount(grid, n, r_q, c_q);
-        count += downCount(grid, n, r_q, c_q);
+        count += leftCount(obstacles, n,r_q ,c_q);
+        count += rightCount(obstacles, n, r_q, c_q);
+        count += upCount(obstacles, n, r_q, c_q);
+        count += downCount(obstacles, n, r_q, c_q);
 
-        count += leftUpCount(grid, n,r_q ,c_q);
-        count += leftDownCount(grid, n,r_q ,c_q);
-        count += rightUpCount(grid, n,r_q ,c_q);
-        count += rightDownCount(grid, n,r_q ,c_q);
+        count += leftUpCount(obstacles, n,r_q ,c_q);
+        count += leftDownCount(obstacles, n,r_q ,c_q);
+        count += rightUpCount(obstacles, n,r_q ,c_q);
+        count += rightDownCount(obstacles, n,r_q ,c_q);
 
 
 
@@ -50,9 +43,9 @@ public class QueensAttack {
         return count;
     }
 
-    private static int rightDownCount(int[][] grid, int n, int r_q, int c_q) {
+    private static int rightDownCount(int[][] obstacles, int n, int r_q, int c_q) {
         int count =0 ;
-        while(c_q+1 < n && r_q+1 < n && grid[r_q+1][c_q+1]==0) {
+        while(c_q+1 < n && r_q+1 < n && !isObstacles(obstacles, n, r_q+1, c_q+1)) {
             count++;
             c_q++;
             r_q++;
@@ -60,9 +53,9 @@ public class QueensAttack {
         return count;
     }
 
-    private static int leftDownCount(int[][] grid, int n, int r_q, int c_q) {
+    private static int leftDownCount(int[][] obstacles, int n, int r_q, int c_q) {
         int count =0 ;
-        while(c_q-1 >=0 && r_q+1 < n && grid[r_q+1][c_q-1]==0) {
+        while(c_q-1 >=0 && r_q+1 < n && !isObstacles(obstacles, n, r_q+1, c_q-1)) {
             count++;
             c_q--;
             r_q++;
@@ -70,18 +63,18 @@ public class QueensAttack {
         return count;
     }
 
-    private static int rightCount(int[][] grid, int n, int r_q, int c_q) {
+    private static int rightCount(int[][] obstacles, int n, int r_q, int c_q) {
         int count =0 ;
-        while(c_q+1 < n && grid[r_q][c_q+1]==0) {
+        while(c_q+1 < n && !isObstacles(obstacles, n, r_q, c_q+1)) {
             count++;
             c_q++;
         }
         return count;
     }
 
-    private static int rightUpCount(int[][] grid, int n, int r_q, int c_q) {
+    private static int rightUpCount(int[][] obstacles, int n, int r_q, int c_q) {
         int count =0 ;
-        while(c_q+1 < n && r_q-1 >=0 && grid[r_q-1][c_q+1]==0) {
+        while(c_q+1 < n && r_q-1 >=0 && !isObstacles(obstacles, n, r_q-1, c_q+1)) {
             count++;
             c_q++;
             r_q--;
@@ -89,37 +82,37 @@ public class QueensAttack {
         return count;
     }
 
-    private static int downCount(int[][] grid, int n, int r_q, int c_q) {
+    private static int downCount(int[][] obstacles, int n, int r_q, int c_q) {
         int count =0 ;
-        while(r_q+1 < n && grid[r_q+1][c_q]==0) {
+        while(r_q+1 < n && !isObstacles(obstacles, n, r_q+1, c_q)) {
             count++;
             r_q++;
         }
         return count;
     }
 
-    private static int upCount(int[][] grid, int n, int r_q, int c_q) {
+    private static int upCount(int[][] obstacles, int n, int r_q, int c_q) {
         int count =0 ;
-        while(r_q-1 >= 0 && grid[r_q-1][c_q]==0) {
+        while(r_q-1 >= 0 && !isObstacles(obstacles, n, r_q-1, c_q)) {
             count++;
             r_q--;
         }
         return count;
     }
 
-    private static int leftCount(int[][] grid, int n, int r_q, int c_q) {
+    private static int leftCount(int[][] obstacles, int n, int r_q, int c_q) {
         int count =0 ;
-        while(c_q-1 >=0 && grid[r_q][c_q-1]==0) {
+        while(c_q-1 >=0 &&  !isObstacles(obstacles, n, r_q, c_q-1)) {
             count++;
             c_q--;
         }
         return count;
     }
 
-    private static int leftUpCount(int[][] grid, int n, int r_q, int c_q) {
+    private static int leftUpCount(int[][] obstacles, int n, int r_q, int c_q) {
 
         int count =0 ;
-        while(c_q-1 >=0 && r_q-1 >=0 && grid[r_q-1][c_q-1]==0) {
+        while(c_q-1 >=0 && r_q-1 >=0 && !isObstacles(obstacles, n, r_q-1, c_q-1)) {
             count++;
             c_q--;
             r_q--;
@@ -141,22 +134,21 @@ public class QueensAttack {
     }
 
 
-    private static void fillObstacles(int[][] grid, int[][] obstacles, int n) {
-
-        final int OBSTACLE = 1;
+    private static boolean isObstacles(int[][] obstacles, int n, int r_q, int c_q) {
 
         for (int[] obstacle : obstacles) {
-//            System.out.println("obstacle = " + obstacle);
             int row_x_i = obstacle[0];
             int col_y_j = obstacle[1];
-
             // Since the grid is computer defined array ,
             // Hence transform from bottom up to top down
             row_x_i = (n-1) - (row_x_i -1);
             col_y_j = col_y_j -1;
 
-            grid[row_x_i][col_y_j] = OBSTACLE;
+            if(row_x_i == r_q && col_y_j == c_q) {
+                return true;
+            }
         }
+        return false;
 
     }
 
@@ -188,14 +180,6 @@ public class QueensAttack {
         k =0 ;
         r_q = 4187;
         c_q = 5068;
-
-        byte[][] grid = new byte [n][];
-        for (int i = 0; i < n; i++) {
-            grid[i] = new byte [n];
-            for (int j = 0; j < n; j++) {
-                grid[i][j] =0;
-            }
-        }
 
         int testcase3 = queensAttack(n, k, r_q, c_q, obstacles);
 
